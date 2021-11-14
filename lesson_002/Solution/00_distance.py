8843291e67861f4e20267ac1cd6b2e6f603d80b4
Balabes <zzziggi@gmail.com>
2021-11-14 12:36:00 +0300

    task 1

--- lesson_002/Solution/00_distance.py
+++ lesson_002/Solution/00_distance.py
@@ -12,9 +12,24 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances = {
+}
 
-# TODO здесь заполнение словаря
+moscow_london_dist = ((sites['Moscow'][0]-sites['London'][0])**2 + (sites['Moscow'][1]-sites['London'][1])**2)**0.5
+moscow_paris_dist = ((sites['Moscow'][0]-sites['Paris'][0])**2 + (sites['Moscow'][1]-sites['Paris'][1])**2)**0.5
+london_paris_dist = ((sites['London'][0]-sites['Paris'][0])**2 + (sites['London'][1]-sites['Paris'][1])**2)**0.5
+
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london_dist
+distances['Moscow']['Paris'] = moscow_paris_dist
+
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_london_dist
+distances['London']['Paris'] = london_paris_dist
+
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_paris_dist
+distances['Paris']['London'] = london_paris_dist
 
 print(distances)
 
