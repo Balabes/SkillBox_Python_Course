766a8ddcf7dc3fba7684cc650daa6f77eda6098d
Balabes <zzziggi@gmail.com>
2021-11-17 20:43:25 +0300

    8 task

--- lesson_003/Solution/07_wall.py
+++ lesson_003/Solution/07_wall.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
+sd.resolution = (700, 700)
 
-# TODO здесь ваш код
+y = -50
+x = -100
+brick_height = 50
+brick_width = 100
+for i in range(14):
+    y += brick_height
+    start_point = sd.get_point(0, y)
+    stop_point = sd.get_point(700, y)
+    sd.line(start_point, stop_point, sd.COLOR_YELLOW, 1)
+    if i % 2 == 0:
+        x = -100
+        for _ in range(7):
+            x += brick_width
+            start_point = sd.get_point(x, y)
+            stop_point = sd.get_point(x, y + brick_height)
+            sd.line(start_point, stop_point, sd.COLOR_YELLOW, 1)
+    if i % 2 != 0:
+        x = -50
+        for _ in range(7):
+            x += brick_width
+            start_point = sd.get_point(x, y)
+            stop_point = sd.get_point(x, y + brick_height)
+            sd.line(start_point, stop_point, sd.COLOR_YELLOW, 1)
 
-simple_draw.pause()
+sd.pause()
