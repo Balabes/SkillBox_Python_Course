cd8e7c7cc83b6c416fa597a0979b74f45884fb5d
Balabes <zzziggi@gmail.com>
2021-11-17 21:00:25 +0300

    9 task

--- lesson_003/Solution/08_smile.py
+++ lesson_003/Solution/08_smile.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
+sd.resolution = (700, 700)
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+def draw_smile(x, y, color):
+    """Ебать серьезный"""
+    center_point = sd.get_point(x, y)
+    sd.circle(center_point, 100, color, 2)
+    left_eye = sd.get_point(x - 50, y + 60)
+    sd.circle(left_eye, 10, color, 2)
+    right_eye = sd.get_point(x + 50, y + 60)
+    sd.circle(right_eye, 10, color, 2)
+    nose_start = sd.get_point(x, y + 50)
+    nose_stop = sd.get_point(x, y + 10)
+    sd.line(nose_start, nose_stop, color, 2)
+    mouth_start = sd.get_point(x - 40, y - 50)
+    mouth_stop = sd.get_point(x + 40, y - 50)
+    sd.line(mouth_start, mouth_stop, color, 2)
+
+
+for _ in range(10):
+    point = sd.random_point()
+    draw_smile(point.x, point.y, sd.COLOR_ORANGE)
+
+sd.pause()
