b311e399c9ff2561bdb198f6f266f2034dfc8de8
Balabes <zzziggi@gmail.com>
2021-11-17 18:36:47 +0300

    3 task, half

--- lesson_003/Solution/02_envelop.py
+++ lesson_003/Solution/02_envelop.py
@@ -20,7 +20,12 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x > paper_x and envelop_y > paper_y:
+    print("Поместилось")
+elif envelop_x > paper_y and envelop_y > paper_x:
+    print("Поместилось")
+else:
+    print("Не поместилось")
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,8 +33,8 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -48,5 +53,3 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 6, 11
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
-
-# TODO здесь ваш код
