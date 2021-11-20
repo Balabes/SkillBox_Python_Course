0eef651b6a697139c2577bddb14c1b288243c05e
Balabes <zzziggi@gmail.com>
2021-11-20 21:37:14 +0300

    5 task

--- lesson_004/Solution/05_snowfall.py
+++ lesson_004/Solution/05_snowfall.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+import random as rnd
 
+screen_x = 1200
+screen_y = 600
+
+sd.resolution = (screen_x, screen_y)
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
@@ -10,6 +15,40 @@ import simple_draw as sd
 
 N = 20
 
+snowflakes_list = []
+for _ in range(N):
+    snowflakes_list.append(
+        [rnd.randint(0, screen_x), rnd.randint(screen_y - 200, screen_y + 400), rnd.randint(10, 100), True])
+    #   Координата снежинки х         Координата снежинки screen_y                Длина лучиков     Снежинка падает
+    #                                                                                               или уже упала
+
+
+# print(snowflakes_list)
+
+
+def snowflake_fall(snowflakes):
+    """Лагает как тварь где-то после ~40 снежинок"""
+    while True:
+        sd.clear_screen()
+        for snowflake in snowflakes:
+            point = sd.get_point(snowflake[0], snowflake[1])
+            sd.snowflake(center=point, length=snowflake[2], color=sd.COLOR_WHITE)
+            if snowflake[3]:
+                if snowflake[1] <= 0:
+                    snowflake[1] = 0
+                    snowflake[3] = False
+                    snowflakes_list.append(
+                        [rnd.randint(0, screen_x), rnd.randint(screen_y - 200, screen_y + 400), rnd.randint(10, 100),
+                         True])
+                else:
+                    snowflake[1] -= 5
+                    snowflake[0] += rnd.randint(-5, 5)
+        sd.sleep(0.05)
+        if sd.user_want_exit():
+            break
+
+
+snowflake_fall(snowflakes_list)
 # Пригодятся функции
 # sd.get_point()
 # sd.snowflake()
@@ -17,15 +56,15 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+# y = 500
+# while y > 0:
+#     sd.clear_screen()
+#     point = sd.get_point(300, y)
+#     sd.snowflake(center=point, length=40, color=sd.COLOR_WHITE)
+#     y -= 5
+#     sd.sleep(0.05)
+#     if sd.user_want_exit():
+#         break
 
 sd.pause()
 
@@ -43,5 +82,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-
