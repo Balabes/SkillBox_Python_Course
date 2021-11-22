6056c9fae8afd8a2db577de9ee14978f402871ee
Balabes <zzziggi@gmail.com>
2021-11-22 12:52:50 +0300

    lesson 5 task 3

--- lesson_005/Solution/03_burger.py
+++ lesson_005/Solution/03_burger.py
@@ -15,4 +15,39 @@
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
-# TODO здесь ваш код
+import my_burger as ingredients
+
+
+def do_double_cheeseburger():
+    print("Double cheeseburger")
+    ingredients.add_bun()
+    ingredients.add_ketchup()
+    ingredients.add_steak()
+    ingredients.add_cheese()
+    ingredients.add_steak()
+    ingredients.add_cheese()
+    ingredients.add_onion()
+    ingredients.add_pickle()
+    ingredients.add_ketchup()
+    ingredients.add_mustard()
+    ingredients.add_bun()
+
+
+def do_my_burger():
+    print("My burger")
+    ingredients.add_bun()
+    ingredients.add_ketchup()
+    ingredients.add_mustard()
+    ingredients.add_mayo()
+    ingredients.add_salad()
+    ingredients.add_steak()
+    ingredients.add_cheese()
+    ingredients.add_bacon()
+    ingredients.add_fried_onion()
+    ingredients.add_pickle()
+    ingredients.add_tomato()
+    ingredients.add_bun()
+
+
+do_double_cheeseburger()
+do_my_burger()
---
+++ lesson_005/Solution/my_burger.py
@@ -0,0 +1,46 @@
+def add_bun():
+    print("Добавляем булочку")
+
+
+def add_steak():
+    print("Добавляем котлетку")
+
+
+def add_pickle():
+    print("Добавляем огурки")
+
+
+def add_tomato():
+    print("Добавляем помидорк")
+
+
+def add_mayo():
+    print("Добавляем мазик")
+
+
+def add_cheese():
+    print("Добавляем СЫЫЫЫЫЫЫЫЫр")
+
+
+def add_ketchup():
+    print("Добавляем кептчуп")
+
+
+def add_mustard():
+    print("Добавляем горчицу")
+
+
+def add_onion():
+    print("Добавляем лучок")
+
+
+def add_fried_onion():
+    print("Добавляем карамелизованный лучок")
+
+
+def add_bacon():
+    print("Добавляем бекон")
+
+
+def add_salad():
+    print("Добавляем лист салата")
