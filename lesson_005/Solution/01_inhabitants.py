8499a58ada9c57e1e0ecc1214003d4d87d63a759
Balabes <zzziggi@gmail.com>
2021-11-22 11:25:15 +0300

    lesson 5 task 1

--- lesson_005/Solution/01_inhabitants.py
+++ lesson_005/Solution/01_inhabitants.py
@@ -2,5 +2,16 @@
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
+from room_1 import folks as r1_folks
+from room_2 import folks as r2_folks
 
-# TODO здесь ваш код
+result_string = "В комнате room_1 живут:"
+
+for person in r1_folks:
+    result_string = result_string + " " + person
+print(result_string)
+
+result_string = "В комнате room_2 живёт:"
+for person in r2_folks:
+    result_string = result_string + " " + person
+print(result_string)
