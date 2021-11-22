0384e94067b4f3bdd96119231b007964de5f2913
Balabes <zzziggi@gmail.com>
2021-11-22 11:49:17 +0300

    lesson 5 task 2

--- lesson_005/Solution/02_district.py
+++ lesson_005/Solution/02_district.py
@@ -5,8 +5,22 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.soviet_street.house1.room1 import folks as sovok_h1_r1
+from district.soviet_street.house1.room2 import folks as sovok_h1_r2
+from district.soviet_street.house2.room1 import folks as sovok_h2_r1
+from district.soviet_street.house2.room2 import folks as sovok_h2_r2
 
+from district.central_street.house1.room1 import folks as centr_h1_r1
+from district.central_street.house1.room2 import folks as centr_h1_r2
+from district.central_street.house2.room1 import folks as centr_h2_r1
+from district.central_street.house2.room2 import folks as centr_h2_r2
 
+population_census = "На районе живет следующий биомусор: "
 
+peoples_of_district = [sovok_h1_r1, sovok_h1_r2, sovok_h2_r1, sovok_h2_r2, centr_h1_r1, centr_h1_r2, centr_h2_r2,
+                       centr_h2_r1]
 
+for room in peoples_of_district:
+    population_census += ','.join(room) + ","
+
+print(population_census)
