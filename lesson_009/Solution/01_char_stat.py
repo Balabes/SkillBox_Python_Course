1a9e0f245af876bcec10dc5a8b5cfa2c01da195f
Balabes <zzziggi@gmail.com>
2021-12-07 13:30:45 +0300

    Lesson 009 посчитал буквы

--- lesson_009/Solution/01_char_stat.py
+++ lesson_009/Solution/01_char_stat.py
@@ -20,9 +20,49 @@
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import zipfile
+from pprint import pprint
 
-# TODO здесь ваш код
 
+class TextAnalyzer:
+    def __init__(self, src_file_name):
+        self.statistic = {}
+        self.file_name = None
+        self.src_file_name = src_file_name
+        self.file = None
+        self.alpha_counter = 0
+
+    def get_file(self):
+        if self.src_file_name.endswith('.zip'):
+            zfile = zipfile.ZipFile(self.src_file_name, 'r')
+            for filename in zfile.namelist():
+                zfile.extract(filename)
+            self.file_name = filename
+        else:
+            self.file_name = self.src_file_name
+        # self.file = open(self.file_name, mode='r', encoding='cp1251')
+        # print(self.file.readline())
+
+    def calculate_static(self):
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char in self.statistic:
+                            self.statistic[char] += 1
+                        else:
+                            self.statistic[char] = 1
+        for key, item in self.statistic.items():
+            self.alpha_counter += item
+        pprint(self.statistic)
+        print(f"Итого букв = {self.alpha_counter}")
+
+
+
+file_path = "voyna-i-mir.txt"
+ta = TextAnalyzer(file_path)
+ta.get_file()
+ta.calculate_static()
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
