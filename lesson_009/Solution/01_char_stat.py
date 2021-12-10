c5e55f9c3d0f7654067ef4429c3eb09df2db88e5
Balabes <zzziggi@gmail.com>
2021-12-10 21:47:40 +0300

    хуета с абстракциями

--- lesson_009/Solution/01_char_stat.py
+++ lesson_009/Solution/01_char_stat.py
@@ -21,11 +21,13 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 import zipfile
+from abc import ABCMeta, abstractmethod
 
 
-class TextAnalyzer:
+class TextAnalyzer(metaclass=ABCMeta):
     def __init__(self, src_file_name):
         self.statistic = {}
+        self.sorted_static = []
         self.file_name = None
         self.src_file_name = src_file_name
         self.file = None
@@ -36,9 +38,9 @@ class TextAnalyzer:
             self.file_name = self.src_file_name
         else:
             zfile = zipfile.ZipFile(self.src_file_name, 'r')
-            for filename in zfile.namelist():
-                zfile.extract(filename)
-            self.file_name = filename
+            filename_in_zip = zfile.namelist()
+            zfile.extract(filename_in_zip[0])
+            self.file_name = filename_in_zip[0]
         # self.file = open(self.file_name, mode='r', encoding='cp1251')
         # print(self.file.readline())
 
@@ -58,20 +60,50 @@ class TextAnalyzer:
         print('+---------+----------+')
         print('|  буква  | частота  |')
         print('+---------+----------+')
-        for items in self.statistic.items():
+        for items in self.sorted_static:
             print('|{:^9}|{:^10}|'.format(items[0], items[1]))
+            # print(items)
         print('+---------+----------+')
         print('|  Итого  |{:^10}|'.format(self.alpha_counter))
         print('+---------+----------+')
 
+    @abstractmethod
+    def sort_result(self):
+        pass
 
-file_path = "voyna-i-mir.txt"
-ta = TextAnalyzer(file_path)
-ta.get_file()
-ta.get_alpha_static()
-ta.print_static()
-# После выполнения первого этапа нужно сделать упорядочивание статистики
-#  - по частоте по возрастанию
-#  - по алфавиту по возрастанию
-#  - по алфавиту по убыванию
-# Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
+    def do_analyze(self):
+        self.get_file()
+        self.get_alpha_static()
+        self.sort_result()
+        self.print_static()
+
+
+class TAFreqSortUp(TextAnalyzer):
+    def sort_result(self):
+        self.sorted_static.clear()
+        self.sorted_static = sorted(self.statistic.items(), key=lambda x: x[1], )
+
+
+class TAFreqSortDown(TextAnalyzer):
+    def sort_result(self):
+        self.sorted_static.clear()
+        self.sorted_static = sorted(self.statistic.items(), key=lambda x: x[1], reverse=True)
+
+    class TAInAlphabetOrderSortDown(TextAnalyzer):
+        def sort_result(self):
+            self.sorted_static.clear()
+            self.sorted_static = sorted(self.statistic.items())
+
+    class TAInAlphabetOrderSortUp(TextAnalyzer):
+        def sort_result(self):
+            self.sorted_static.clear()
+            self.sorted_static = sorted(self.statistic.items(), reverse=True)
+
+    file_path = "voyna-i-mir.txt"
+    ta = TAFreqSortUp(file_path)
+    ta.do_analyze()
+    # После выполнения первого этапа нужно сделать упорядочивание статистики
+    #  - по частоте по возрастанию
+    #  - по алфавиту по возрастанию
+    #  - по алфавиту по убыванию
+    # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
