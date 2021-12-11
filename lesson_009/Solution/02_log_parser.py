467891433c6072665b053f6f0de703aeb4b82689
Balabes <zzziggi@gmail.com>
2021-12-11 16:00:36 +0300

    task 2 (какая-то чушь)

--- lesson_009/Solution/02_log_parser.py
+++ lesson_009/Solution/02_log_parser.py
@@ -21,15 +21,77 @@
 
 from abc import ABCMeta, abstractmethod
 
-src_file_name = "events.txt"
+eventstxt = "events.txt"
+outtxt = "out.txt"
 
 
 class LogParser(metaclass=ABCMeta):
-    def __init__(self, _src_file_name, _result_file_name):
-        self.src_file = _src_file_name
-        self.result_file = _result_file_name
+    @abstractmethod
+    def group(self):
+        pass
+
+    def __init__(self, src_file_name, result_file_name):
+        self._src_file = src_file_name
+        self._result_file = result_file_name
+        self._result = {}
+        self.group_by_minutes = 17
+        self.group_by_hours = 14
+        self.group_by_days = 11
+        self.group_by_month = 8
+        self.group_by_years = 5
+        self._group_by = None
+        self.group()
+
+    def get_stat(self):
+        with open(self._src_file, 'r', encoding='utf8') as file:
+            for line in file:
+                date = line[:self._group_by] + "]"
+                if "NOK" in line:
+                    if date in self._result.keys():
+                        self._result[date] += 1
+                    else:
+                        self._result[date] = 1
+
+    def print_result(self):
+        with open(self._result_file, 'w', encoding='utf8') as file:
+            for items in self._result.items():
+                file.write(f"{items[0]} {items[1]}\r")
+                print(items)
 
 
+class LogParserGroupByMinutes(LogParser):
+    def group(self):
+        self._group_by = self.group_by_minutes
+
+
+class LogParserGroupByHours(LogParser):
+    def group(self):
+        self._group_by = self.group_by_hours
+
+
+class LogParserGroupByDays(LogParser):
+    def group(self):
+        self._group_by = self.group_by_days
+
+
+class LogParserGroupByMonths(LogParser):
+    def group(self):
+        self._group_by = self.group_by_month
+
+
+class LogParserGroupByYears(LogParser):
+    def group(self):
+        self._group_by = self.group_by_years
+
+
+# lp = LogParserGroupByMinutes(eventstxt, outtxt)
+lp = LogParserGroupByHours(eventstxt, outtxt)
+# lp = LogParserGroupByMonths(eventstxt, outtxt)
+# lp = LogParserGroupByDays(eventstxt, outtxt)
+# lp = LogParserGroupByYears(eventstxt, outtxt)
+
+lp.get_stat()
+lp.print_result()
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
