3afdfc3a3ea9c614e826d74ade06800e312fe8f7
Balabes <zzziggi@gmail.com>
2021-12-11 22:02:17 +0300

    lesson 10 task 1

--- lesson_010/Solution/01_fifth_element.py
+++ lesson_010/Solution/01_fifth_element.py
@@ -4,17 +4,19 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+while True:
+    try:
+        input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+        leeloo = int(input_data[4])
+        result = BRUCE_WILLIS * leeloo
+        print(f"- Leeloo Dallas! Multi-pass № {result}!")
+    except ValueError as ve:
+        print(f"ValueError, {ve}. Take another try!")
+    except IndexError as ie:
+        print(f"IndexError, {ie}. Take another try!")
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
-
