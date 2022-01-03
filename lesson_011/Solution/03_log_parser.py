9df832e0e55f2319b64d5c1639b74a9a5bb41c0c
Balabes <zzziggi@gmail.com>
2022-01-03 15:44:01 +0300

    lesson 11 task 3

--- lesson_011/Solution/03_log_parser.py
+++ lesson_011/Solution/03_log_parser.py
@@ -13,5 +13,31 @@
 # на консоли должно появится что-то вроде
 #
 # [2018-05-17 01:57] 1234
+# [2018-05-14 19:38:25.873687] NOK
+from collections import OrderedDict
 
-# TODO здесь ваш код
+src_file = "events.txt"
+time_pos = len("[2018-05-17 01:57")
+result_pos = len("[2018-05-14 19:38:25.873687] ")
+
+
+def log_parser_generator(filename):
+    events = OrderedDict()
+    key = None
+    with open(filename, 'r', encoding='utf8') as file:
+        for line in file:
+            key = line[:time_pos] + "]"
+            char = line[result_pos:result_pos + 1]
+            if char == 'N':
+                if key not in events:
+                    events[key] = 1
+                    # yield events.popitem()
+                else:
+                    events[key] += 1
+
+
+# grouped_events = log_parser_generator(src_file)
+# for group_time, event_count in grouped_events:
+#     print(f'[{group_time}] {event_count}')
+
+log_parser_generator(src_file)
